70c6b01bd41886751c5fb00e8c1afd91c259b102
Evgeniy Bogdanov <bogdanov.jn@gmail.com>
2019-12-25 00:39:51 +0200

    Updates for expansion panel

--- core/lib/components/expansionPanel/expansionPanelSummary.js
+++ core/lib/components/expansionPanel/expansionPanelSummary.js
@@ -1,3 +1,43 @@
 // import { offset } from 'worktube-theme/core';
-const MuiExpansionPanelSummary = {};
+
+const MuiExpansionPanelSummary = {
+    root: {
+        // display: flex;
+        padding: 0
+        // min-height: 48px;
+        // transition: min-height 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    }
+    // .MuiExpansionPanelSummary-root:hover:not(.Mui-disabled) {
+    //     cursor: pointer;
+    // }
+    // .MuiExpansionPanelSummary-root.Mui-expanded {
+    //     min-height: 64px;
+    // }
+    // .MuiExpansionPanelSummary-root.Mui-focused {
+    //     background-color: #e0e0e0;
+    // }
+    // .MuiExpansionPanelSummary-root.Mui-disabled {
+    //     opacity: 0.38;
+    // }
+    // .MuiExpansionPanelSummary-content {
+    //     margin: 12px 0;
+    //     display: flex;
+    //     flex-grow: 1;
+    //     transition: margin 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    // }
+    // .MuiExpansionPanelSummary-content.Mui-expanded {
+    //     margin: 20px 0;
+    // }
+    // .MuiExpansionPanelSummary-expandIcon {
+    //     transform: rotate(0deg);
+    //     transition: transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    // }
+    // .MuiExpansionPanelSummary-expandIcon:hover {
+    //     background-color: transparent;
+    // }
+    // .MuiExpansionPanelSummary-expandIcon.Mui-expanded {
+    //     transform: rotate(180deg);
+    // }
+};
+
 export default MuiExpansionPanelSummary;
