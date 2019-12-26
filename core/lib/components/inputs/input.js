e647fa1a12ad78a3374771e83c0d5ea81901bd29
Evgeniy Bogdanov <bogdanov.jn@gmail.com>
2019-12-26 21:21:42 +0200

    Updates for inputs v3

--- core/lib/components/inputs/input.js
+++ core/lib/components/inputs/input.js
@@ -1,4 +1,4 @@
-import { offset } from 'worktube-theme/core';
+import { offset, border } from 'worktube-theme/core';
 
 const MuiInput = {// .MuiInput-root {
   //     position: relative;
@@ -8,7 +8,7 @@ const MuiInput = {// .MuiInput-root {
       marginTop: offset('12'),
       marginBottom: offset('12')
     }
-  }
+  },
   //   .MuiInput-underline:after {
   //     left: 0;
   //     right: 0;
@@ -27,19 +27,26 @@ const MuiInput = {// .MuiInput-root {
   //     transform: scaleX(1);
   //     border-bottom-color: #E03C3B;
   //   }
-  //   .MuiInput-underline:before {
-  //     left: 0;
-  //     right: 0;
-  //     bottom: 0;
-  //     content: "\00a0";
-  //     position: absolute;
-  //     transition: border-bottom-color 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-  //     border-bottom: 1px solid rgba(0, 0, 0, 0.42);
-  //     pointer-events: none;
-  //   }
-  //   .MuiInput-underline:hover:not(.Mui-disabled):before {
-  //     border-bottom: 2px solid rgba(0, 0, 0, 0.7);
-  //   }
+  underline: {
+    '&:before:': {
+      // left: 0;
+      // right: 0;
+      // bottom: 0;
+      // content: "\00a0";
+      // position: absolute;
+      // transition: border-bottom-color 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+      // borderBottom: 1px solid rgba(0, 0, 0, 0.42);
+      borderBottom: border['1px'].solid.dark.lighten3
+      // pointer-events: none;
+    },
+    '&:hover': {
+      '&:not(.Mui-disabled)': {
+        '&:before:': {
+          borderBottom: border['1px'].solid.dark.default
+        }
+      }
+    }
+  },
   //   .MuiInput-underline.Mui-disabled:before {
   //     border-bottom-style: dotted;
   //   }
