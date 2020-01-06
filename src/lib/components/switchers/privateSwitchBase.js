5a487b619e4a728421ea67b6f01c94b9dc042eb0
Evgeniy Bogdanov <bogdanov.jn@gmail.com>
2020-01-06 13:13:01 +0200

    Updates for switchers

---
+++ src/lib/components/switchers/privateSwitchBase.js
@@ -0,0 +1,22 @@
+import { offset } from 'worktube-theme/core'
+  
+const PrivateSwitchBase = {
+    'root-614': {
+        padding: offset('4'),
+        borderRadius: offset('9')
+    }
+    // .PrivateSwitchBase-input-617 {
+    //     top: 0;
+    //     left: 0;
+    //     width: 100%;
+    //     cursor: inherit;
+    //     height: 100%;
+    //     margin: 0;
+    //     opacity: 0;
+    //     padding: 0;
+    //     z-index: 1;
+    //     position: absolute;
+    // }
+}
+
+export default PrivateSwitchBase
---
+++ src/lib/components/switchers/switch.js
@@ -0,0 +1,118 @@
+import {
+    offset
+  } from 'worktube-theme/core'
+  
+const MuiSwitch = {
+    root: {
+        width: offset('27'),
+        height: offset('18')
+        // display: inline-flex;
+        // padding: 12px;
+        // z-index: 0;
+        // overflow: hidden;
+        // position: relative;
+        // box-sizing: border-box;
+        // flex-shrink: 0;
+        // vertical-align: middle;
+    }
+//   .MuiSwitch-edgeStart {
+//     margin-left: -8px;
+//   }
+//   .MuiSwitch-edgeEnd {
+//     margin-right: -8px;
+//   }
+//   .MuiSwitch-switchBase {
+//     top: 0;
+//     left: 0;
+//     color: #fafafa;
+//     z-index: 1;
+//     position: absolute;
+//     transition: left 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+//   }
+//   .MuiSwitch-switchBase.Mui-checked {
+//     transform: translateX(50%);
+//   }
+//   .MuiSwitch-switchBase.Mui-disabled {
+//     color: #bdbdbd;
+//   }
+//   .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track {
+//     opacity: 0.5;
+//   }
+//   .MuiSwitch-switchBase.Mui-disabled + .MuiSwitch-track {
+//     opacity: 0.12;
+//   }
+//   .MuiSwitch-colorPrimary.Mui-checked {
+//     color: #2DAFBC;
+//   }
+//   .MuiSwitch-colorPrimary.Mui-disabled {
+//     color: #bdbdbd;
+//   }
+//   .MuiSwitch-colorPrimary.Mui-checked + .MuiSwitch-track {
+//     background-color: #2DAFBC;
+//   }
+//   .MuiSwitch-colorPrimary.Mui-disabled + .MuiSwitch-track {
+//     background-color: #000000;
+//   }
+//   .MuiSwitch-colorPrimary.Mui-checked:hover {
+//     background-color: rgba(45, 175, 188, 0.12);
+//   }
+//   @media (hover: none) {
+//     .MuiSwitch-colorPrimary.Mui-checked:hover {
+//       background-color: transparent;
+//     }
+//   }
+//   .MuiSwitch-colorSecondary.Mui-checked {
+//     color: #00E8ED;
+//   }
+//   .MuiSwitch-colorSecondary.Mui-disabled {
+//     color: #bdbdbd;
+//   }
+//   .MuiSwitch-colorSecondary.Mui-checked + .MuiSwitch-track {
+//     background-color: #00E8ED;
+//   }
+//   .MuiSwitch-colorSecondary.Mui-disabled + .MuiSwitch-track {
+//     background-color: #000000;
+//   }
+//   .MuiSwitch-colorSecondary.Mui-checked:hover {
+//     background-color: rgba(0, 232, 237, 0.12);
+//   }
+//   @media (hover: none) {
+//     .MuiSwitch-colorSecondary.Mui-checked:hover {
+//       background-color: transparent;
+//     }
+//   }
+//   .MuiSwitch-sizeSmall {
+//     width: 40px;
+//     height: 24px;
+//     padding: 7px;
+//   }
+//   .MuiSwitch-sizeSmall .MuiSwitch-thumb {
+//     width: 16px;
+//     height: 16px;
+//   }
+//   .MuiSwitch-sizeSmall .MuiSwitch-switchBase {
+//     padding: 4px;
+//   }
+//   .MuiSwitch-input {
+//     left: -100%;
+//     width: 300%;
+//   }
+//   .MuiSwitch-thumb {
+//     width: 20px;
+//     height: 20px;
+//     box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12);
+//     border-radius: 50%;
+//     background-color: currentColor;
+//   }
+//   .MuiSwitch-track {
+//     width: 100%;
+//     height: 100%;
+//     opacity: 0.38;
+//     z-index: -1;
+//     transition: opacity 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+//     border-radius: 7px;
+//     background-color: #000000;
+//   }
+}
+
+export default MuiSwitch
