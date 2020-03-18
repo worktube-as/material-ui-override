03382b623290b985851b4fa102075abd77012ebe
Evgeniy Bogdanov <evgeniybogdanov@MacBook-Pro-Evgeniy.local>
2020-03-18 14:14:58 +0200

    Tab updates

---
+++ core/lib/components/tabs/tab.js
@@ -0,0 +1,81 @@
+import { offset } from 'worktube-theme/core';
+const MuiTab = {
+// .MuiTab-root {
+//     color: rgba(0, 0, 0, 0.7);
+//     padding: 6px 12px;
+//     overflow: hidden;
+//     position: relative;
+//     font-size: 1.4rem;
+//     max-width: 264px;
+//     min-width: 72px;
+//     box-sizing: border-box;
+//     min-height: 48px;
+//     text-align: center;
+//     flex-shrink: 0;
+//     font-family: "Montserrat", sans-serif;
+//     font-weight: 600;
+//     line-height: 2rem;
+//     white-space: normal;
+//     letter-spacing: 0px;
+//     text-transform: uppercase;
+//   }
+  '@media (min-width:600px)': {
+    root: {
+    //   font-size: 1.3rem;
+      minWidth: offset('36')
+    }
+  }
+//   .MuiTab-labelIcon {
+//     min-height: 72px;
+//     padding-top: 9px;
+//   }
+//   .MuiTab-labelIcon .MuiTab-wrapper > *:first-child {
+//     margin-bottom: 6px;
+//   }
+//   .MuiTab-textColorInherit {
+//     color: inherit;
+//     opacity: 0.7;
+//   }
+//   .MuiTab-textColorInherit.Mui-selected {
+//     opacity: 1;
+//   }
+//   .MuiTab-textColorInherit.Mui-disabled {
+//     opacity: 0.5;
+//   }
+//   .MuiTab-textColorPrimary {
+//     color: rgba(0, 0, 0, 0.53);
+//   }
+//   .MuiTab-textColorPrimary.Mui-selected {
+//     color: #2DAFBC;
+//   }
+//   .MuiTab-textColorPrimary.Mui-disabled {
+//     color: rgba(0, 0, 0, 0.18);
+//   }
+//   .MuiTab-textColorSecondary {
+//     color: rgba(0, 0, 0, 0.53);
+//   }
+//   .MuiTab-textColorSecondary.Mui-selected {
+//     color: #00E8ED;
+//   }
+//   .MuiTab-textColorSecondary.Mui-disabled {
+//     color: rgba(0, 0, 0, 0.18);
+//   }
+//   .MuiTab-fullWidth {
+//     flex-grow: 1;
+//     max-width: none;
+//     flex-basis: 0;
+//     flex-shrink: 1;
+//   }
+//   .MuiTab-wrapped {
+//     font-size: 1.2rem;
+//     line-height: 1.5;
+//   }
+//   .MuiTab-wrapper {
+//     width: 100%;
+//     display: inline-flex;
+//     align-items: center;
+//     flex-direction: column;
+//     justify-content: center;
+//   }
+};
+export default MuiTab;
