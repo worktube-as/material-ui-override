1f495ed25b9d563cd6f78f511bf476b33e528c95
Evgeniy Bogdanov <evgeniybogdanov@MacBook-Pro-Evgeniy.local>
2020-03-31 12:31:16 +0300

    New breakpoints

--- core/index.js
+++ core/index.js
@@ -1,3 +1,4 @@
+import breakpoint from './lib/root/breakpoint';
 import palette from './lib/root/palette';
 import typography from './lib/root/typography';
 import shadow from './lib/root/shadow';
@@ -6,6 +7,7 @@ import transitions from './lib/root/transitions';
 import zIndexes from './lib/root/zIndex';
 import components from './lib/components';
 const worktubeMaterialTheme = {
+  breakpoint: breakpoint,
   palette: palette,
   typography: typography,
   shadow: shadow,
---
+++ core/lib/root/breakpoint.js
@@ -0,0 +1,18 @@
+// import { zIndex } from 'worktube-theme/core';
+const breakpoint = {
+    keys: {
+        0: 'xs',
+        1: 'sm',
+        2: 'md',
+        3: 'lg',
+        4: 'xl'
+    },
+    values: {
+        xs: 0,
+        sm: 576,
+        md: 768,
+        lg: 992,
+        xl: 1200
+    }
+};
+export default breakpoint;
